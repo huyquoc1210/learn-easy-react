4e20cadf0b3f995fe2a1025ab6cc69fc58071dc9
huyquoc1210 <contact.huyquoc@gmail.com>
2023-09-17 06:39:32 +0700

    Practice conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MainLayout } from './components/Layout';
 import { Widget } from './components/common';
 import { MyText } from './features/labs/Mytext';
@@ -7,6 +7,13 @@ import { Student } from './models';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
 
   const john: Student = {
     name: 'John',
@@ -18,31 +25,34 @@ function App() {
     console.log('student Click', student);
   }
 
+  // if (loading) return <p>loading...</p>;
+
   return (
     <>
+      {loading && <p>loading...</p>}
+      {loading ? <p>Loading...</p> : <p>data ready!</p>}
+      {true && 'show true'}
+      {false && 'show false'}
+      {[].length > 0 && 'Show 0'}
+      {Boolean(NaN) && 'Show NaN'}
       <MainLayout>
         <StudentCar student={john} onClick={handleStudentClick} />
       </MainLayout>
-
       <MyText>TypeScript</MyText>
       <MyText>{18}</MyText>
       <MyText>{null}</MyText>
       <MyText>{undefined}</MyText>
       <MyText>{false}</MyText>
-
       <MyText>
         <span>Type</span>
       </MyText>
-
       <MyText>
         <span>Type</span> script
       </MyText>
-
       <MyText>
         <span>Type</span>
         <span>Script</span>
       </MyText>
-
       <div>
         <div>
           <Widget title="Earning Overview">Chart1</Widget>
