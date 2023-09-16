54ef649fbcef7420af613eeeca7e11bd365dcdd3
huyquoc1210 <contact.huyquoc@gmail.com>
2023-09-17 05:43:10 +0700

    Sử dụng props.children - Layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Footer, Header } from './components/common';
 import { StudentCar } from './features/labs/Student';
 import { Student } from './models';
 import { MyText } from './features/labs/Mytext';
+import { MainLayout } from './components/Layout';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,14 +20,16 @@ function App() {
 
   return (
     <>
-      <Header></Header>
-      <StudentCar student={john} onClick={handleStudentClick} />
-      <Footer>TypeScript</Footer>
+      <MainLayout>
+        <StudentCar student={john} onClick={handleStudentClick} />
+      </MainLayout>
+
+      <MyText>TypeScript</MyText>
       <MyText>{18}</MyText>
       <MyText>{null}</MyText>
       <MyText>{undefined}</MyText>
       <MyText>{false}</MyText>
-      
+
       <MyText>
         <span>Type</span>
       </MyText>
@@ -39,8 +42,6 @@ function App() {
         <span>Type</span>
         <span>Script</span>
       </MyText>
-    
-
     </>
   );
 }
